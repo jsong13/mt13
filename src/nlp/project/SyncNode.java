14a894694c7d0ed67fa0640f80b8894031e14765
jlsong <js6855@nyu.edu>
2013-12-08 06:23:59 +0000

    sync grammar rules and trees

---
+++ src/nlp/project/SyncNode.java
@@ -0,0 +1,54 @@
+package nlp.project;
+
+import java.util.*;
+import java.util.Collections.*;
+import java.io.*;
+import nlp.util.Pair;
+import nlp.ling.Tree;
+
+// TreeNode used in a synctree or elsewhere
+// non-terminals are unnamed in this class
+// only the number and the orderings are recorded here
+// T is the type of sentence words
+
+class SyncNode<T>{
+  // for future use
+  T name = null;
+
+  // represent a rule of synchronus grammar: T -> e1e2 A B e3 C, f1 B f2 C A
+  // record the order change from target to source
+  // t2s[0] = 1, t2s[1] = 2,  t2s[2] = 0,
+  List<Integer> ordert2s = new ArrayList<Integer>(); 
+
+  // length = length(NT) + 1
+  // use "" to represent empty string
+  List<List<T>> paddingSrc = new ArrayList<List<T>>();
+  List<List<T>> paddingTrg = new ArrayList<List<T>>();
+
+  public int numNonTerminals(){return ordert2s.size();}
+
+  // dump for debug
+  public String toString(){
+    String ret = "<X|";
+    
+    int i=0;
+    for (List<T> pads : paddingSrc) {
+      for (T p : pads) ret += (" "+p);
+      if (i<ordert2s.size())
+        ret += " "+String.format("X%d", i+1);
+      i++;
+    }
+    ret += "|"; 
+    i=0;
+    for (List<T> pads : paddingTrg) {
+      for (T p : pads) ret += " "+p;
+      if (i<ordert2s.size())
+        ret += " "+String.format("X%d", ordert2s.get(i)+1);
+      i++;
+    }
+    ret += ">";
+ 
+    ret = ret.replaceAll("\\s+", " ");
+    return ret;
+  }
+}
---
+++ src/nlp/project/SyncTrees.java
@@ -0,0 +1,31 @@
+package nlp.project;
+
+import java.util.*;
+import java.util.Collections.*;
+import java.io.*;
+import nlp.util.Pair;
+import nlp.ling.Tree;
+
+class SyncTrees {
+  static <T> Tree<T> reduce2Src(Tree<SyncNode<T>> syncTree, T name){
+    SyncNode<T> sn = syncTree.getLabel();
+    Tree<T> ret = new Tree<T>(name);
+    List<Tree<T>> newchildren =  new ArrayList<Tree<T>>();
+    int i = 0;
+    for(List<T> padding : sn.paddingSrc) {
+      if (!padding.isEmpty()){
+        for (T p : padding) {
+          newchildren.add(new Tree<T>(p));
+        }
+      }
+      if (i<sn.numNonTerminals()){
+        newchildren.add(reduce2Src(syncTree.getChildren().get(i), name));
+      }
+      i++;
+    }
+
+    if (! newchildren.isEmpty()) 
+      ret.setChildren(newchildren);
+    return ret;
+  }
+}
