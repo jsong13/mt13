240a612abda1a565f7ad40d399a1b87f10c6f605
jlsong <js6855@nyu.edu>
2013-12-20 00:40:17 -0500

    add constructor to read from string

--- src/nlp/project/SyncNode.java
+++ src/nlp/project/SyncNode.java
@@ -12,56 +12,112 @@ import nlp.ling.Tree;
 // T is the type of sentence words
 
 class SyncNode<T>{
-  // for future use
+  // for future, not used now
   T name = null;
+  
+  // for dump
+  static final String NT = "NT-";
+  final T dummy = (T)new Object();
 
   // represent a rule of synchronus grammar: T -> e1e2 A B e3 C, f1 B f2 C A
   // record the order change from target to source
   // t2s[0] = 1, t2s[1] = 2,  t2s[2] = 0,
-  List<Integer> ordert2s = new ArrayList<Integer>(); 
+  List<Integer> ordert2s;
 
   // length = length(NT) + 1
   // use "" to represent empty string
-  List<List<T>> paddingSrc = new ArrayList<List<T>>();
-  List<List<T>> paddingTrg = new ArrayList<List<T>>();
+  List<List<T>> paddingSrc; 
+  List<List<T>> paddingTrg; 
 
   public int numNonTerminals(){return ordert2s.size();}
 
-  // dump for debug
   public String toString(){
     String ret = "";
 
-    ret += toSourceString();
+    ret += toSrcString();
     ret += "|"; 
-    ret += toTargetString();
+    ret += toTrgString();
  
     return ret;
   }
 
-  public String toSourceString(){
+  public String toSrcString(){
     String ret = "";
     int i=0;
     for (List<T> pads : paddingSrc) {
       for (T p : pads) ret += (" "+p);
       if (i<ordert2s.size())
-        ret += " "+String.format("_X_%d", i+1);
+        ret += " "+String.format("%s%d", NT, i+1);
       i++;
     }
     ret = ret.replaceAll("\\s+", " ").trim();
     return ret;
   }
 
-  public String toTargetString(){
+  public String toTrgString(){
     String ret = "";
     int i=0;
     for (List<T> pads : paddingTrg) {
       for (T p : pads) ret += " "+p;
       if (i<ordert2s.size())
-        ret += " "+String.format("_X_%d", ordert2s.get(i)+1);
+        ret += " "+String.format("%s%d", NT, ordert2s.get(i)+1);
       i++;
     }
     ret = ret.replaceAll("\\s+", " ").trim();
     return ret;
   }
+  
+  public SyncNode(){
+    paddingSrc = new ArrayList<List<T>>();
+    paddingTrg = new ArrayList<List<T>>();
+    ordert2s = new ArrayList<Integer>();
+  }
+  // load from string that dumped by toSourceString and toTargetString
+  // in case of srcString only, target padding are filled with empty string
+  // and order is set to linear
+  public SyncNode(String srcString){
+    this();
+    String[] parts = srcString.trim().split("\\s+");
 
+    List<T> currentPadding = new ArrayList<T>();
+    for (int i =0; i<parts.length; i++) {
+      if (parts[i].startsWith(NT)){
+        paddingSrc.add(currentPadding);
+        currentPadding = new ArrayList<T>();
+        ordert2s.add(-1);
+        continue;
+      } else {
+        currentPadding.add(toT(parts[i])); 
+      }
+    }
+    paddingSrc.add(currentPadding);
+  }
+
+  // see above
+  public SyncNode(String srcString, String trgString){
+    this(srcString); 
+
+    int j = 0;
+    String[] parts = trgString.trim().split("\\s+");
+    List<T> currentPadding = new ArrayList<T>();
+    for(int i=0; i < parts.length; i++) {
+      if (parts[i].startsWith(NT)) {
+        paddingTrg.add(currentPadding);
+        currentPadding = new ArrayList<T>();
+        ordert2s.set(j, -1+Integer.valueOf(parts[i].substring(NT.length())));
+        j++;
+      } else {
+        currentPadding.add(toT(parts[i])); 
+      }
+    }
+    paddingTrg.add(currentPadding);
+  }
+
+  // convert String to Type T
+  private T toT(String a){
+    if (dummy.getClass().getName().equals("java.lang.Integer")) {
+      return (T)(Integer.valueOf(a));
+    } 
+    return (T)a;
+  }
 }
