61a3f62e5056d710ffc4035d0dafc78e4b46b8ef
jlsong <js6855@nyu.edu>
2013-12-20 00:41:23 -0500

    preorder traversal of labels

--- src/nlp/project/SyncTrees.java
+++ src/nlp/project/SyncTrees.java
@@ -7,6 +7,7 @@ import nlp.util.Pair;
 import nlp.ling.Tree;
 
 class SyncTrees {
+
   static <T> Tree<T> reduce2Src(Tree<SyncNode<T>> syncTree, T name){
     Tree<T> ret = new Tree<T>(name);
     SyncNode<T> sn = syncTree.getLabel();
@@ -29,6 +30,23 @@ class SyncTrees {
     return ret;
   }
   
-  
+  // return a list of labels not subtrees
+  static <T> List<T> getPreOrderTraversal(Tree<T> tree) {
+    List<T> ret = new ArrayList<T>();
+    getPreOrderTraversalHelper(tree, ret);
+    return ret;
+  }
  
+  private static <T> void getPreOrderTraversalHelper(Tree<T> tree, List<T> ret) {
+    // just in case
+    if (tree == null) return;
+
+    ret.add(tree.getLabel());
+    for (Tree<T> c : tree.getChildren()) {
+      getPreOrderTraversalHelper(c, ret);
+    }
+  }
+
+
+
 }
